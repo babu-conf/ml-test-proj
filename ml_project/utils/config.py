5181749ff0686c3d3d30fe27937f55ca155882ea
Babu Turlapati <51709640+babu-conf@users.noreply.github.com>
2026-03-02 10:40:10 -0500

    add utils/config.py

---
+++ ml_project/utils/config.py
@@ -0,0 +1,5 @@
+FEATURE_COLS = ["CARAT", "CUT", "COLOR", "CLARITY", "DEPTH", "TABLE_PCT", "X", "Y", "Z"]
+CAT_COLS = ["CUT", "COLOR", "CLARITY"]
+NUM_COLS = ["CARAT", "DEPTH", "TABLE_PCT", "X", "Y", "Z"]
+MODEL_NAME = "diamond_price_classifier"
+VERSION_NAME = "git_repo_v1"
